e23eef4ff4b213ddb3e4255fba40c74218600fa3
Daria Kriukova <kda.dariush@gmail.com>
2022-09-20 23:54:53 +0300

    refactor: upd syntax, remove excess code
    redundant

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const itemCountSpan = document.getElementById("item-count");
 const uncheckedCountSpan = document.getElementById("unchecked-count");
 let todos = [];
 
-function newTodo() {
+const newTodo = () => {
   let text = prompt("Enter new task to do", "Do something");
   const todo = {
     text, checked: false, id: Date.now(),
@@ -19,53 +19,51 @@ function newTodo() {
   renderTodo(todo);
 }
 
-function toggleTodoCheck(key) {
-  const index = todos.findIndex(el => el.id === Number(key));
-  todos[index].checked = !todos[index].checked;
-  updateCounters();
+const toggleTodoCheck = (key) => {
+  todos = todos.map(todo => {
+    if (todo.id !== key) return todo
+    return {...todo, checked: !todo.checked}
+  });
+  submitChanges();
+}
+
+const renderTodo = (item) => {
+  const li = document.createElement("li");
+  li.setAttribute("id", `${item.id}`);
+  li.setAttribute("class", `${classNames.TODO_ITEM}`)
+  li.innerHTML = `<input class="${classNames.TODO_CHECKBOX}" onClick="toggleTodoCheck(${item.id})" type="checkbox" ${item.checked ? "checked" : ""}>
+                  <label class="${classNames.TODO_TEXT}"><span>${item.text}</span></label>
+                  <button class="${classNames.TODO_DELETE}" onClick="deleteTodo(${item.id})">delete</button>`;
+  list.appendChild(li);
+  submitChanges();
+}
+
+const deleteTodo = (key) => {
+  const li = document.getElementById(`${key}`);
+  li?.remove();
+  todos = todos.filter(item => item.id !== Number(key));
+  submitChanges();
+}
+
+const updateStorage = () => {
   localStorage.setItem('todos', JSON.stringify(todos));
 }
 
-function renderTodo(item) {
-  if (item?.deleted) {
-    const li = document.getElementById(`${item.id}`);
-    li?.remove();
-  } else {
-    const li = document.createElement("li");
-    li.setAttribute("id", `${item.id}`);
-    li.setAttribute("class", `${classNames.TODO_ITEM}`)
-    li.innerHTML = `<input class="${classNames.TODO_CHECKBOX}" onClick="toggleTodoCheck(${item.id})" type="checkbox" ${item.checked ? "checked" : ""}>
-                    <label class="${classNames.TODO_TEXT}"><span>${item.text}</span></label>
-                    <button class="${classNames.TODO_DELETE}" onClick="deleteTodo(${item.id})">delete</button>`;
-    list.appendChild(li);
-  }
-  updateCounters();
-  localStorage.setItem('todos', JSON.stringify(todos));
-}
-
-function deleteTodo(key) {
-  const index = todos.findIndex(item => item.id === Number(key));
-  if (index >= 0) {
-    const todo = {
-      deleted: true, id: key,
-      ...todos[index]
-    };
-    todos = todos.filter(item => item.id !== Number(key));
-    renderTodo(todo);
-  }
-}
-
-function updateCounters() {
+const updateCounters = () => {
   itemCountSpan.textContent = todos.length.toString();
   uncheckedCountSpan.textContent = todos.filter((todoEl) => todoEl.checked === false).length.toString();
 }
 
+const submitChanges = () => {
+  updateStorage();
+  updateCounters();
+}
+
 document.addEventListener('DOMContentLoaded', () => {
   const ref = localStorage.getItem('todos');
-  if (ref) {
-    todos = JSON.parse(ref);
-    todos.forEach(t => {
-      renderTodo(t);
-    });
-  }
+  if (!ref) return;
+  todos = JSON.parse(ref);
+  todos.forEach(t => {
+    renderTodo(t);
+  });
 });
